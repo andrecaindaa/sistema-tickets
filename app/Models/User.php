0c4bf73cef0f2e5793f2da850abf1185fb56fc71
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-02-16 18:36:22 +0000

    Adiciona sistema de roles (operador e cliente)

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,15 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function isOperador()
+    {
+        return $this->role === 'operador';
+    }
+
+    public function isCliente()
+    {
+        return $this->role === 'cliente';
+    }
+
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,1 +1,1 @@
-<h1>Seja Bem Vindo Admin</h1>
+<h1>Seja Bem Vindo Admin, esta é a nossa página de boas vindas</h1>
