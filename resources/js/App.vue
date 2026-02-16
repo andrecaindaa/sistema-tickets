cfe722c4340bc2bdc7210441a86d758a29b4c35d
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-02-16 17:45:12 +0000

    Initial commit - Laravel base setup

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Sistema de Tickets</h1>
+  </div>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div style="padding:40px; font-size:30px;">
+        🚀 Sistema de Tickets a funcionar!
+    </div>
+</template>
+
+<script setup>
+</script>
