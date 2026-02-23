b370b849fa2674f10afbc4a6c7c898509885eea1
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-02-23 17:21:04 +0000

    seed: add default ticket types

---
+++ database/seeders/TicketTipoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TicketTipoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\TicketTipo::insert([
+            ['nome' => 'Reserva'],
+            ['nome' => 'Suporte Técnico'],
+            ['nome' => 'Informação'],
+        ]);
+    }
+}
